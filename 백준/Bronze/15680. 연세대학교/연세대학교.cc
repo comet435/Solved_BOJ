3ec8b2e4f3826f611fde5eb790866fd1fff4bf3f
윤신웅 <112417993+DrYoon04@users.noreply.github.com>
2023-11-21 20:08:50 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    cin >> num;
+    if (num == 0)
+        cout << "YONSEI";
+
+    else
+        cout << "Leading the Way to the Future";
+    return 0;
+
+}
