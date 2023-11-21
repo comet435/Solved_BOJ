2cfd99de3a6e139fa08e49aa9391f54a5a962e24
윤신웅 <112417993+DrYoon04@users.noreply.github.com>
2023-11-21 20:02:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 124 ms, Memory: 2840 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+struct Point {
+    int x, y;
+};
+
+bool cmp(const Point& p1, const Point& p2) {
+    if (p1.x == p2.x) {
+        return p1.y < p2.y;
+    }
+    return p1.x < p2.x;
+}
+
+int main() {
+    int num = 0;
+    std::cin >> num;
+
+    Point* points = new Point[num];
+
+    for (int i = 0; i < num; ++i) {
+        std::cin >> points[i].x >> points[i].y;
+    }
+
+    std::sort(points, points + num, cmp);
+
+    for (int i = 0; i < num; ++i) {
+        std::cout << points[i].x << " " << points[i].y << '\n';
+    }
+
+    delete[] points;
+
+    return 0;
+}
